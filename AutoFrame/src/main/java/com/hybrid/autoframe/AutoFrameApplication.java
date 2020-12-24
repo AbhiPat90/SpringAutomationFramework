e6472cff95fbe63d94bd151cdb67f94ea3e443e7
Abhishek Patre <abhishek.patre@shutterfly.com>
2020-12-24 23:06:59 +0530

    Spring Structure

---
+++ AutoFrame/src/main/java/com/hybrid/autoframe/AutoFrameApplication.java
@@ -0,0 +1,13 @@
+package com.hybrid.autoframe.AutoFrame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoFrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoFrameApplication.class, args);
+	}
+
+}
---
+++ AutoFrame/src/test/java/com/hybrid/autoframe/AutoFrameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hybrid.autoframe.AutoFrame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoFrameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
